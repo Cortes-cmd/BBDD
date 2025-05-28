024b515ea6211ea9effb73c97e765a525ab10de0
Cortes-cmd <alejandro.cortesdiaz24@campusfp.es>
2025-05-28 10:49:33 +0200

    Tarea con api LM

--- 3ºTrimestre/Ejercicios/NodeJSConexionMongoDB/public/script.js
+++ 3ºTrimestre/Ejercicios/NodeJSConexionMongoDB/public/script.js
@@ -1,4 +1,4 @@
-fetch('/api/peliculas')
+/*fetch('/api/peliculas')
   .then(res => res.json())
   .then(data => {
     const contenedor = document.getElementById('peliculas');
@@ -8,3 +8,21 @@ fetch('/api/peliculas')
       contenedor.appendChild(div);
     });
   });
+*/
+
+function mostrarVista(vista) {
+document.querySelector('.vista1').style.display = vista === 1 ? 'block' : 'none';
+document.querySelector('.vista2').style.display = vista === 2 ? 'block' : 'none';
+document.querySelector('.vista3').style.display = vista === 3 ? 'block' : 'none';
+}
+
+function filtrar() {
+let filtro = document.getElementById('filtro').value.toLowerCase();
+let tarjetas = document.querySelectorAll('.tarjeta');
+tarjetas.forEach(t => {
+t.style.display = t.textContent.toLowerCase().includes(filtro) ? 'block' : 'none';
+});
+}
+
+    // Mostrar vista 1 por defecto
+    mostrarVista(2);
